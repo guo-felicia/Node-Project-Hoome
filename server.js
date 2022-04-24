c74a4abdfc9b90624022abcd13254c931c33faf6
tongfeiguo <tongfei.gf@gmail.com>
2022-04-24 15:39:40 -0400

    add fav controller to server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import mongoose from "mongoose";
 import questionsController from "./Questions/questions-controller.js";
 import reviewsController from "./Reviews/reviews-controller.js";
+import favoritesController from "./Favorites/favorites-controller.js";
 
 mongoose.connect(`mongodb+srv://felicia:felicia726@cluster0.uovsx.mongodb.net/Hoome?retryWrites=true&w=majority`);
 const app = express();
@@ -14,5 +15,6 @@ app.get('/', (req, res) => {
 app.use(express.json());
 questionsController(app);
 reviewsController(app);
+favoritesController(app);
 //Heroku
 app.listen(process.env.PORT || 4000);
