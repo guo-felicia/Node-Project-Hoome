0d5541650a65ad44b6453a42c91385eee509342c
tongfeiguo <tongfei.gf@gmail.com>
2022-04-24 21:08:55 -0400

    delete pricing in schema

--- Favorites/favorites-schema.js
+++ Favorites/favorites-schema.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 const schema = mongoose.Schema(
     {
         address: String,
-        price: Object,
         rating: Number,
         images: Array,
         name: String,
