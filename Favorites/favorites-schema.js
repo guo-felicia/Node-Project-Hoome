fbdfa46f5e8e93350b10cacf5509faea2fe801e6
tongfeiguo <tongfei.gf@gmail.com>
2022-04-24 21:07:37 -0400

    add price to fav schema

--- Favorites/favorites-schema.js
+++ Favorites/favorites-schema.js
@@ -3,11 +3,12 @@ import mongoose from 'mongoose';
 const schema = mongoose.Schema(
     {
         address: String,
+        price: Object,
         rating: Number,
         images: Array,
         name: String,
         bathrooms: Number,
         bedrooms: Number,
-        beds:Number
+        beds: Number
     }, {collection: 'favorites'});
 export default schema;
